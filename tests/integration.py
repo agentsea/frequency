894beeeab5ab6f405fc111c6aa03fd64c0b5bd74
Patrick Barker <patrickbarkerco@gmail.com>
2024-01-08 10:05:47 -0700

    fix build

--- tests/integration.py
+++ tests/integration.py
@@ -1,47 +1,68 @@
 import sys
+import time
 
 sys.path.append("../frequency")
 
 from frequency.client.v1.frequency_api import FrequencyAPI
 from frequency.provider.runpod import RunPodProvider
 
+GPU_ENABLED = True
+
+MODEL = "facebook/opt-350m"
+NAME = "opt"
+LORA = "ybelkada/opt-350m-lora"
+
+if GPU_ENABLED:
+    MODEL = "Qwen/Qwen-VL-Chat"
+    NAME = "qwen"
+    LORA = ""
+
 # provider = RunPodProvider()
 
 # provider.run("test-freq", "")
 
+# https://l88g599jicx8xx-8000.proxy.runpod.net/
 
-client = FrequencyAPI(endpoint="http://localhost:8000")
+client = FrequencyAPI(endpoint="https://l88g599jicx8xx-8000.proxy.runpod.net/")
+# client = FrequencyAPI(endpoint="http://localhost:8000")
 
 resp = client.get_health()
 print("health: ", resp)
 
-body = {
-    "hf_repo": "facebook/opt-350m",
-    "name": "opt",
-    "type": "AutoModelForCausalLM",
-    "cuda": False,
-}
-resp = client.load_model(body)
-print("response from load model: ", resp)
+# body = {
+#     "hf_repo": MODEL,
+#     "name": NAME,
+#     "type": "AutoModelForCausalLM",
+#     "cuda": GPU_ENABLED,
+# }
+# resp = client.load_model(body)
+# print("response from load model: ", resp)
+time.sleep(5)
+
+# body = {
+#     "model": NAME,
+#     "hf_repo": LORA,
+#     "name": "adapter_1",
+# }
+# resp = client.load_adapter(body)
+# print("response from loading adapter: ", resp)
+
+# body = {
+#     "query": "Hello",
+#     "adapters": ["adapter_1"],
+# }
+# resp = client.chat_model(NAME, body)
+# print("response from chat: ", resp)
 
 body = {
-    "model": "opt",
-    "hf_repo": "ybelkada/opt-350m-lora",
-    "name": "adapter_1",
+    "query": "Hello how are you?",
 }
-resp = client.load_adapter(body)
-print("response from loading adapter: ", resp)
-
-body = {
-    "query": "Hello",
-    "adapters": ["adapter_1"],
-}
-resp = client.chat_model("opt", body)
+resp = client.chat_model(NAME, body)
 print("response from chat: ", resp)
 
 
-resp = client.get_adapters()
-print("get adapters: ", resp)
+# resp = client.get_adapters()
+# print("get adapters: ", resp)
 
-resp = client.get_models()
-print("get models: ", resp)
+# resp = client.get_models()
+# print("get models: ", resp)
